a9164192f14577639f15752e1d7015a12e0c7a70
2Glitch27 <chiter1019@yandex.ru>
2026-09-26 04:42:37 +0300

    сервисы

---
+++ services.php
@@ -0,0 +1,23 @@
+<?php require 'doctype.php'; ?>
+<section class="section services" id="services">
+    <div class="container">
+       <div class="page-hero__content">
+         <h1 class="page-hero__title">
+            SERVICES
+        </h1>
+        <div class="page-hero_desc">
+            If you are looking for a full-service 
+            construction company, look to Createx 
+            Construction Bureau. We are doing our 
+            best to be a partner for all of your construction needs.
+        </div>
+       </div>
+       <div class="page-hero__media">
+        <img src="" alt="" class="page-hero__img">
+       </div>
+    </div>
+</section>
+
+
+</body>
+</html>
